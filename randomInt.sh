44433ac755ae5da6e5f2df2dcf3d0cf41941134e
xenoCoder01 <spamtemp03@gmail.com>
2019-07-13 13:52:25 +0530

    new_ui

--- randomInt.sh
+++ randomInt.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+clear
 range=0
 inp=0
-moves=1
+moves=0
 RED='\e[1;31m'
 GREEN='\e[1;32m'
 BLUE='\e[1;34m'
-echo RANDOM NUMBER GUESSER!!!!!
+figlet -t -c -f pagga RANDOM NUMBER GUESSER!!!!!
 read -p "Input the range: " range
 randomInt=$((($RANDOM % $range)+1)) 
 #echo $randomInt
 until [ $inp -eq $randomInt ]; do
     printf "$BLUE""Guess the number: "
     read inp
-    [ $inp -eq $randomInt ] || ([ $inp -gt $randomInt ] && echo -e "$RED" && toilet -t --font future "It is less than $inp") || (echo -e "$RED" && toilet -t --font future "It is greater than $inp")
+    [ $inp -eq $randomInt ] || ([ $inp -gt $randomInt ] && echo -e "$RED" && toilet -t --font future "Too High" && echo "") || (echo -e "$RED" && toilet -t --font future "Too Low" && echo "")
     let "moves++"
 done
-echo -e "$GREEN""Hurrah!!! You Guessed it in $moves Moves"
+echo -e "$GREEN" && toilet -t --font pagga "Hurrah!!!"
+toilet -t --font pagga "You Guessed"
+toilet -t --font pagga "it in"
+toilet -t --font pagga "$moves Moves"
 read -p "Press enter to continue...." abc
